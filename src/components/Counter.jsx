cdc0654e193328aa1fae31c647c6ce1c125c20a1
Sergey Kuznetsov <mr.enline@gmail.com>
2022-10-10 17:27:18 +0300

    release connect react with redux

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,3 +1,8 @@
+import { connect } from 'react-redux';
+//import { inc, dec, rnd } from './../actions';
+import * as actions from './../actions';
+import { bindActionCreators } from 'redux';
+
 const Counter = ({ counter, inc, dec, rnd }) => {
     return (
         <div className="jumbotron">
@@ -15,4 +20,57 @@ const Counter = ({ counter, inc, dec, rnd }) => {
     );
 };
 
-export default Counter;
+//пример реализации на классовых компонентах
+// class Counter extends components {
+//     render() {
+//         { counter, inc, dec, rnd } = this.props;
+//         return (
+//             <div className="jumbotron">
+//                 <h1>{counter}</h1>
+//                 <button onClick={dec} className="btn btn-primary">
+//                     DEC
+//                 </button>
+//                 <button onClick={inc} className="btn btn-primary">
+//                     INC
+//                 </button>
+//                 <button onClick={rnd} className="btn btn-primary">
+//                     RND
+//                 </button>
+//             </div>
+//         );
+//     }
+// }
+
+const mapStateToProps = (state) => {
+    return {
+        counter: state.value,
+    };
+};
+
+//1-й вариант реализации функции
+// const mapDispatchToProps = (dispatch) => {
+//     return {
+//         inc: () => dispatch(inc()),
+//         dec: () => dispatch(dec()),
+//         rnd: () => dispatch(rnd(Math.floor(Math.random() * 10))),
+//     };
+// };
+
+//2-й вариант реализации функции
+// const mapDispatchToProps = (dispatch) => {
+//     const { inc, dec, rnd } = actions;
+//     return {
+//         inc: () => dispatch(inc()),
+//         dec: () => dispatch(dec()),
+//         rnd: () => dispatch(rnd()),
+//     };
+// };
+
+//3-й вариант реализации функции через bindActionCreators
+// const mapDispatchToProps = (dispatch) => {
+//     return bindActionCreators(actions, dispatch);
+// };
+
+//export default connect(mapStateToProps, mapDispatchToProps)(Counter);
+//экспорт ниже аналогичен закомментированному экспорту выше
+export default connect(mapStateToProps, actions)(Counter);
--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "./index.css";
-import { configureStore } from "@reduxjs/toolkit";
-import { legacy_createStore, bindActionCreators } from "@reduxjs/toolkit";
-import reducer from "./reducer";
-import { Provider } from "react-redux";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import { legacy_createStore } from '@reduxjs/toolkit';
+import reducer from './reducer';
+import { Provider } from 'react-redux';
+import App from './components/App';
 
 const store = legacy_createStore(reducer);
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+ReactDOM.createRoot(document.getElementById('root')).render(
     <Provider store={store}>
         <React.StrictMode>
             <App />
         </React.StrictMode>
-    </Provider>,
+    </Provider>
 );
